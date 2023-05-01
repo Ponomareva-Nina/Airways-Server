f60abb9664c9bb478b169ccd2b3329d517f0b025
Ponomareva Nina <nina.9412@mail.ru>
2023-05-02 01:50:35 +0300

    add endpoints for creating and getting users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './users.model';
+import { createUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private UserDB: typeof User) {}
+
+  async createUser(dto: createUserDto) {
+    const user = await this.UserDB.create(dto);
+    return user;
+  }
+
+  async getAllUsers() {
+    const users = await this.UserDB.findAll();
+    return users;
+  }
+
+  // async getUser(id: number) {
+  //   const users = await this.UserDB.findOne(id);
+  //   return users;
+  // }
+}
